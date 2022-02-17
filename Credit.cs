a7db86b0fd5a3652d5d90f1ee4a1fbb828168735
timelessnesses <mooping3roblox@gmail.com>
2022-02-17 16:23:45 +0700

    ah

---
+++ Credit.cs
@@ -0,0 +1,16 @@
+﻿
+using StorybrewCommon.Scripting;
+
+namespace StorybrewScripts
+{
+    public class Credit : StoryboardObjectGenerator
+    {
+        public override void Generate()
+        {
+            var credit = GetLayer("").CreateSprite("sb/credit.png");
+            credit.Scale(-2000, 480.0f / GetMapsetBitmap("sb/credit.png").Height);
+            credit.Fade(-2000, -1500, 0, 1);
+            credit.Fade(-1500, -1000, 1, 0);
+        }
+    }
+}
