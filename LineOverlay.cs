92fedeae6c8421eb51da9245f19318af87e55f13
timelessnesses <mooping3roblox@gmail.com>
2022-02-22 11:40:18 +0700

    i wanna commit not living

--- LineOverlay.cs
+++ LineOverlay.cs
@@ -21,34 +21,35 @@ namespace StorybrewScripts
 
         public override void Generate()
         {
-            var times = new Dictionary<int,int>();
+            var times = new Dictionary<int, int>();
             times[78302] = 189930;
             times[245744] = 335046;
-            foreach(var time in times){
-                Generates(time.Key, time.Value);
+            foreach (var time in times)
+            {
+                GenerateX(time.Key, time.Value);
             }
         }
 
-        public void Generates(int StartTime, int EndTime)
+        public void GenerateX(int StartTime, int EndTime)
         {
             var hitobjectLayer = GetLayer("");
             foreach (var hitobject in Beatmap.HitObjects)
             {
-                if ((StartTime != 0 || EndTime != 0) && 
+                if ((StartTime != 0 || EndTime != 0) &&
                     (hitobject.StartTime < StartTime - 5 || EndTime - 5 <= hitobject.StartTime))
                     continue;
 
                 var hSprite = hitobjectLayer.CreateSprite(SpritePath, OsbOrigin.Centre, hitobject.Position);
-                hSprite.Fade(OsbEasing.InOutSine, hitobject.StartTime, hitobject.StartTime+100, 0, 1);
-                hSprite.ScaleVec(OsbEasing.In,hitobject.StartTime,hitobject.StartTime+FadeDuration, new OpenTK.Vector2((int)SpriteScale, (int)SpriteScale), new OpenTK.Vector2((int)SpriteScale, 600000000));
-                hSprite.ScaleVec(OsbEasing.In, hitobject.EndTime, hitobject.EndTime + FadeDuration, new OpenTK.Vector2((int)SpriteScale, 600000000), new OpenTK.Vector2(0, 600000000));
+                hSprite.Fade(OsbEasing.None, hitobject.StartTime-FadeDuration, hitobject.StartTime-200,0 ,1);
+                hSprite.ScaleVec(OsbEasing.In, hitobject.StartTime-FadeDuration, hitobject.StartTime, new OpenTK.Vector2((int)0, (int)0), new OpenTK.Vector2((int)SpriteScale, 1000));
+                hSprite.ScaleVec(OsbEasing.In, hitobject.EndTime, hitobject.EndTime + FadeDuration, new OpenTK.Vector2((int)SpriteScale, 1000), new OpenTK.Vector2(0, 1000));
                 hSprite.Fade(OsbEasing.In, hitobject.EndTime, hitobject.EndTime + FadeDuration, 1, 0);
                 hSprite.Additive(hitobject.StartTime, hitobject.EndTime + FadeDuration);
                 hSprite.Color(hitobject.StartTime, hitobject.Color);
 
                 if (hitobject is OsuSlider)
                 {
-                    hSprite.MoveX(OsbEasing.InOutSine, hitobject.EndTime+FadeDuration, hitobject.EndTime+(FadeDuration*2), hitobject.Position.X, hitobject.Position.X+Random(-2,2));
+                    hSprite.MoveX(OsbEasing.InOutSine, hitobject.EndTime + FadeDuration, hitobject.EndTime + (FadeDuration * 1.2), hitobject.Position.X, hitobject.Position.X + Random(-2, 2));
                     var timestep = Beatmap.GetTimingPointAt((int)hitobject.StartTime).BeatDuration / 128;
                     var startTime = hitobject.StartTime;
                     while (true)
@@ -67,5 +68,6 @@ namespace StorybrewScripts
                 }
             }
         }
+        
     }
 }
